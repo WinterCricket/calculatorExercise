84847ae9955d8fbd4536c914860c3e2bfb70137f
wintercricket <hodhodhod2003@gmail.com>
2021-06-28 00:19:01 -0500

    use the same result let variable inside each function

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
 
 
-sum = document.getElementById("sum-el");
+let sum = document.getElementById("sum-el");
 
 function add(){
-	 result = num1 + num2;
+	 let result = num1 + num2;
 	sum.textContent =  result;
 }
 function subtract(){
-	 result = num1 - num2;
+	 let result = num1 - num2;
 	sum.textContent =  result;
 }
 
 function divide(){
-	result = num1 / num2;
+	let result = num1 / num2;
 	sum.textContent =  result;
 }
 function multiply(){
-	 result = num1 * num2;
+	 let result = num1 * num2;
 	sum.textContent =  result;
 }
